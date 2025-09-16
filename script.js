4bb57de29d2c46c21b6d2434859af3bda36fcfc9
PatrickStarson <spartak.mamiev@gmail.com>
2025-09-16 16:00:38 +0200

    Create json files for skills and projects, then fetch those and populate the html.

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const toggle = document.querySelector('.toggle');
 const modeToggle = document.querySelector('#mode-toggle');
 const modeLabel = document.querySelector('.mode-switch-container label');
 
+const skillsContainer = document.querySelector('.skills-main-container');
+
+const projectsContainer = document.querySelector('.projects-container');
+
 
 const savedTheme = localStorage.getItem('theme');
 if (savedTheme === 'dark') {
@@ -26,3 +30,52 @@ modeToggle.addEventListener('change', () => {
     localStorage.setItem('theme', 'light');
   }
 });
+
+
+
+fetch('skills.json')
+  .then(response => response.json())
+  .then(values => {
+    skillsContainer.innerHTML = '';
+    values.forEach(value => {
+      skillsContainer.innerHTML += `
+          <div class="html skill-container">
+            <img src="${value.icon}" alt="${value.name} icon" />
+            <h1>${value.name}</h1>
+            <p>${value.description}</p>
+          </div>`;
+    });
+  });
+
+
+
+fetch('projects.json')
+  .then(response => response.json())
+  .then(values => {
+    projectsContainer.innerHTML = '';
+    values.forEach( value => {
+      projectsContainer.innerHTML += `
+      <div class="project-card">
+        <div class="project-description-container">
+          <h1>${value.projectName}</h1>
+          <p class="project-description">
+            ${value.projectDescription}
+          </p>
+          <a
+            href="${value.link}"
+            target="_blank"
+            >View The Project</a
+          >
+        </div>
+        <a
+          href="${value.link}"
+          target="_blank"
+          ><img
+            class="project-screenshot"
+            src="${value.image}"
+            alt="${value.projectName} image"
+        /></a>
+      </div>
+      `
+    })
+  })
